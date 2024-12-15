c1cd414e2ad79b847f07f634f923ad06968253d7
Ola Fawzi <olafawzii80@gmail.com>
2024-12-15 20:33:45 +0200

    updates design

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-H5VPWNT4.mjs";import"./chunk-7I2MRGUS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-EJWFW3SM.mjs";import"./chunk-7I2MRGUS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
